3519386270d14ba290148c921e1b41b962643e13
Your Name <you@example.com>
2019-12-23 22:51:31 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ // FrontEnd--------
+ Route::get('/', 'HomeController@index' );
+ Route::get('/trang-chu', 'HomeController@index');
+
+
+
+
+
+// Backend----------
+Route::get('/admin', 'AdminController@index');
+
+Route::get('/dashboard', 'AdminController@show_dashboard');
+
+Route::get('/logout', 'AdminController@logout');
+
+Route::post('/admin-dashboard', 'AdminController@dashboard');
+
+// Route::get('/dashboard1', 'AdminController@index_dashboard');
+
+
+
+
+
+// Category-product
+Route::get('/add-category-product', 'CategoryProduct@add_category_product');
+
+Route::get('/all-category-product', 'CategoryProduct@all_category_product');
+
+Route::get('/edit-category-product/{category_product_id}', 'CategoryProduct@edit_category_product');
+
+Route::get('/delete-category-product/{category_product_id}', 'CategoryProduct@delete_category_product');
+
+Route::get('/unactive-category-product/{category_product_id}', 'CategoryProduct@unactive_category_product');
+
+Route::get('/active-category-product/{category_product_id}', 'CategoryProduct@active_category_product');
+
+Route::post('/save-category-product', 'CategoryProduct@save_category_product');
+
+Route::post('/update-category-product/{category_product_id}', 'CategoryProduct@update_category_product');
+
+
